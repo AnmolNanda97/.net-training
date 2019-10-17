f734b6deddd166718926e96711730ffe3cfd8536
AnmolNanda97 <55699266+AnmolNanda97@users.noreply.github.com>
2019-10-17 16:35:30 +0530

    Shapes Assignment

---
+++ Shapes_Assignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int shape_choice;
+            Console.WriteLine("Please choose the shape");
+            Console.WriteLine("1.Circle\n2.Triangle\n3.Square\n4.Rectangle");
+            shape_choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("_________________________________________________________\n\n");
+
+            switch (shape_choice)
+            {
+                case 1:
+                    Circle circle = new Circle();
+                    break;
+
+                case 2:
+                    Triangle triangle = new Triangle();
+                    break;
+
+                case 3:
+                    Square square = new Square();
+                    break;
+
+                case 4:
+                    Rectangle rectangle = new Rectangle();
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter a valid choice");
+                    break;
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Shapes_Assignment/Shapes.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+
+    class Circle
+    {
+        public Circle()
+        {
+            double radius;
+            Console.WriteLine("Enter the radius of the Circle");
+            Console.Write("Radius:");
+            radius = Convert.ToDouble(Console.ReadLine());
+            double c = 3.14 * radius * radius;
+            Console.WriteLine("The Area of your Circle is {0}", c);
+        }
+    }
+
+
+    class Triangle
+    {
+        public Triangle()
+        {
+            double height;
+            double width;
+            Console.WriteLine("Enter the Height and Width of the Triangle");
+            Console.Write("Height:");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Width:");
+            width = Convert.ToDouble(Console.ReadLine());
+            double t = 0.5 * height * width; ;
+            Console.WriteLine("The Area of your Triangle is {0}", t);
+        }
+    }
+
+    class Square
+    {
+        public Square()
+        {
+            double side;
+            Console.WriteLine("Enter the side of the Square");
+            Console.Write("Side:");
+            side = Convert.ToDouble(Console.ReadLine());
+            double s = side * side;
+            Console.WriteLine("The Area of your Square is {0}", s);
+
+        }
+    }
+
+    class Rectangle
+    {
+        public Rectangle()
+        {
+            double length;
+            double breadth;
+            Console.WriteLine("Enter the Length and Breadth of the Rectangle");
+            Console.Write("Length:");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Breadth:");
+            breadth = Convert.ToDouble(Console.ReadLine());
+            double r = length * breadth;
+            Console.WriteLine("The Area of your Rectangle is {0}", r);
+
+        }
+    }
+}
+
+
+
+
+            
+        
+
+
+       
+
+       
+      
+
+        
